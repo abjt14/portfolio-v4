b3a46f493d67c6631c65d11ebbdeaff89344f746
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2022-02-09 12:07:09 -0800

    removed backdrop filter property from nav to improve performance 2

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -160,17 +160,6 @@
 									0 0 32px rgba(0,0,0,0.05),
 									0 0 64px rgba(0,0,0,0.05);
 			background-color: rgba(255, 255, 255, .15);
-
-			&::after {
-				content: ' ';
-				position: absolute;
-				top: 0;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				background-color: rgba(255, 255, 255, .75);
-				z-index: -1;
-			}
 		}
 
 		#mobile-menu-toggle {
